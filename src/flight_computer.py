d0196a2b3992edb10a86e7b62d42a9b388dbe712
Aaron McBride <151467777+aaron-mcbride@users.noreply.github.com>
2026-01-14 16:07:01 -0800

    Started work on protocol

--- src/flight_computer.py
+++ src/flight_computer.py
@@ -20,7 +20,6 @@ from typing import Dict, List
     # Status Logger
     # Flight computer sensor logger
     # Flight computer valve/servo state logger
-    # Passthrough sensor logger
 
 # Flight Computer Sensors
     # 2x IMU - [accel x (2 bytes), accel y (2 bytes), accel z (2 bytes), gyro x (2 bytes), gyro y (2 bytes), gyro z (2 bytes)] - 12 bytes each
@@ -28,62 +27,93 @@ from typing import Dict, List
     # 2x barometer - 3 bytes each (reads pressure)
     # 2x Temperature sensor - temperature (2 bytes) (one is on power board, one is on sensor board)
     # GPS - [latitude (4 bytes), longitude (4 bytes), altitude (4 bytes), velocity x (2 bytes), velocity y (2 bytes), velocity z (2 bytes), time (4 bytes)] - 22 bytes
-    # ADC - 3 bytes per data point (max of 24 pressure sensors, 24 RTDs, 3 strain gauges)
+    # ADC - 3 bytes per data point (max of 24 pressure sensors, 24 RTDs, 3 strain guages)
+    
     # 3x current sensors - 2 bytes each
 
 # Flight computer valves/servos
     # 12 actuator outputs
     # 8 servo outputs
     
-# Packets
+# Downlink Packets (from FC to CC):
+    # GENERAL FORMAT:
+    # <TAG> <ALT TAG (?)> <DATA>
 
     # Sensor data packet:
-        # Tag: 0x00
         # Length: 52 bytes
         # FORMAT:
-            # IMU 1 accel x (2 bytes)
-            # IMU 1 accel y (2 bytes)
-            # IMU 1 accel z (2 bytes)
-            # IMU 1 gyro x (2 bytes)
-            # IMU 1 gyro y (2 bytes)
-            # IMU 1 gyro z (2 bytes)
-            # IMU 2 accel x (2 bytes)
-            # IMU 2 accel y (2 bytes)
-            # IMU 2 accel z (2 bytes)
-            # IMU 2 gyro x (2 bytes)
-            # IMU 2 gyro y (2 bytes)
-            # IMU 2 gyro z (2 bytes)
-            # Magnetometer 1 mag x (2 bytes)
-            # Magnetometer 1 mag y (2 bytes)
-            # Magnetometer 1 mag z (2 bytes)
-            # Magnetometer 2 mag x (2 bytes)
-            # Magnetometer 2 mag y (2 bytes)
-            # Magnetometer 2 mag z (2 bytes)
-            # Barometer 1 pressure (3 bytes)
-            # Barometer 2 pressure (3 bytes)
-            # Temperature sensor 1 temperature (2 bytes)
-            # Temperature sensor 2 temperature (2 bytes)
-            # Current sensor 1 current (2 bytes)
-            # Current sensor 2 current (2 bytes)
-            # Current sensor 3 current (2 bytes)
-        
+            # <0x00>
+            # <IMU 1 accel x (2 bytes)>
+            # <IMU 1 accel y (2 bytes)>
+            # <IMU 1 accel z (2 bytes)>
+            # <IMU 1 gyro x (2 bytes)>
+            # <IMU 1 gyro y (2 bytes)>
+            # <IMU 1 gyro z (2 bytes)>
+            # <IMU 2 accel x (2 bytes)>
+            # <IMU 2 accel y (2 bytes)>
+            # <IMU 2 accel z (2 bytes)>
+            # <IMU 2 gyro x (2 bytes)>
+            # <IMU 2 gyro y (2 bytes)>
+            # <IMU 2 gyro z (2 bytes)>
+            # <Magnetometer 1 mag x (2 bytes)>
+            # <Magnetometer 1 mag y (2 bytes)>
+            # <Magnetometer 1 mag z (2 bytes)>
+            # <Magnetometer 2 mag x (2 bytes)>
+            # <Magnetometer 2 mag y (2 bytes)>
+            # <Magnetometer 2 mag z (2 bytes)>
+            # <Barometer 1 pressure (3 bytes)>
+            # <Barometer 2 pressure (3 bytes)>
+            # <Temperature sensor 1 temperature (2 bytes)>
+            # <Temperature sensor 2 temperature (2 bytes)>
+            # <Current sensor 1 current (2 bytes)>
+            # <Current sensor 2 current (2 bytes)>
+            # <Current sensor 3 current (2 bytes)>
+
     # GPS data packet
-        # Tag: 0x01
         # Length: 22 bytes
         # FORMAT:
-            # Latitude (4 bytes)
-            # Longitude (4 bytes)
-            # Altitude (4 bytes)
-            # Velocity x (2 bytes)
-            # Velocity y (2 bytes)
-            # Velocity z (2 bytes)
-            # Time (4 bytes)
+            # <0x01>
+            # <Latitude (4 bytes)>
+            # <Longitude (4 bytes)>
+            # <Altitude (4 bytes)>
+            # <Velocity x (2 bytes)>
+            # <Velocity y (2 bytes)>
+            # <Velocity z (2 bytes)>
+            # <Time (4 bytes)>
             
     # ADC data packet
+        # Length: <?> (max 64 bytes)
+        # FORMAT:
+            # <0x02>
+            # <?> (alt tag, 1 byte)
+            # <?> (data, variable length)
+        # Alt tag:
+            # Denotes target packet mapping to use in config (adc_sensor_protocol)
+        # Data:
+            # 3 byte data points ordered according to index in target packet mapping.
+
+            
+    # Status packet
+        # Length: <?> (max 64 bytes)
+        # FORMAT:
+            # <0x03>
+            # <?> (data)
+        # Data packet:
+            # Variable size data points ordered according to index in config (status_protocol)
+            # If "type" of data point is "valve" data is 1 bit (0 = closed, 1 = open)
+            # If "type" of data point is "continuous_servo" data is 2 bytes (map 0 - "max_value" to 0 - "max_speed" deg/s)
+            # If "type" of data point is "linear_servo" data is 2 bytes (map 0 - "max_value" to 0 - 100% extension)
+            # If "type" of data point is "positional_servo" data is 2 bytes (map 0 - "max_value" to 0 - 360 degrees)             
+            
+    # Message packet
+        # Length: 2 bytes
+        # FORMAT:
+            # <0x04>
+            # <?> (message ID, 1 byte)
+        # Message ID:
+            # Denotes specific message ID in config (message_protocol)
     
-    
-    
-    
+            
 
 FLIGHT_COMPUTER_RX_PROTOCOL = {
     "sensor"
