f95eebeda37f6a812381a9c2996ff34ce13c6083
Ayla Crowell <ayla.crowell@gmail.com>
2026-04-01 20:28:25 -0700

    Added heartbeat, logging, threads, shutdown, more settings + one line fix to initialize app in socket

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,25 @@
+###################################################################################################
+# Website Settings
+###################################################################################################
+
+# The rate (in Hz) at which the website update thread runs
+WEBSITE_THREAD_UPDATE_RATE: float = 20.0
+
+# The time (in seconds) after which a user is considered disconnected if no heartbeat is received
+WEBSITE_HEARTBEAT_TIMEOUT: float = 3.0
+
+###################################################################################################
+# General System Settings
+###################################################################################################
+
 # If true the system will simulate hardware
 MOCK_MODE: bool = False
 
 # Timeout for waiting for CTS (clear to send) from radio (in seconds)
 RADIO_CTS_TIMEOUT: float = 1.0
+
+# If true, application prints website status messages to console.
+PRINT_WEBSITE_STATUS: bool = False
+
+# If true, application prints website error messages to console.
+PRINT_WEBSITE_ERRORS: bool = True
--- src/website.py
+++ src/website.py
@@ -1,10 +1,51 @@
 import time, threading
-from typing import Callable
-from flask import Flask, request
+import os
+from typing import Callable, Dict
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
+from .logger import Logger
+from . import settings
+
+# Minimum permissable port value
+MIN_PORT_VALUE = 1
+
+# Maximum permissable port value
+MAX_PORT_VALUE = 65535
+
+# Absolute path to website directory
+_WEBSITE_TOP_DIR_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "website")
+
+# Absolute path to website template folder
+WEBSITE_TEMPLATE_FOLDER_PATH = os.path.join(_WEBSITE_TOP_DIR_PATH, "templates")
+
+# Absolute path to website static folder
+WEBSITE_STATIC_FOLDER_PATH = os.path.join(_WEBSITE_TOP_DIR_PATH, "static")
 
 class Website:
-    def __init__(self, flight_computer=None):
+
+    def _update_website(self) -> None:
+        """
+        Background thread method to periodically check user heartbeats.
+        """
+        interval = 1.0 / settings.WEBSITE_THREAD_UPDATE_RATE
+        while True:
+            with self._lock:
+                if self._shutdown_flag:
+                    break
+                try:
+                    current_time = time.time()
+                    for user, last_heartbeat in dict(self._user_heartbeats).items():
+                        if (current_time - last_heartbeat) > settings.WEBSITE_HEARTBEAT_TIMEOUT:
+                            if settings.PRINT_WEBSITE_STATUS:
+                                print(f"WEBSITE STATUS: User {user} disconnected.")
+                            self._website_logger.log_data([user, "status", "User disconnected"])
+                            del self._user_heartbeats[user]
+                    time.sleep(interval)
+                except Exception as e:
+                    if settings.PRINT_WEBSITE_ERRORS:
+                        print(f"WEBSITE ERROR: Update website error: {e}")
+
+    def __init__(self, port: int, website_log_path: str, flight_computer=None,):
         self.app = Flask(__name__)
         self.socketio = SocketIO(self.app, cors_allowed_origins="*")
 
@@ -20,6 +61,13 @@ class Website:
         self._event_seq = 0
         self._action_seq = 0
         self._lock = threading.Lock()
+        self._port = port
+        self._shutdown_flag = False
+        self._user_heartbeats: Dict[str, float] = {}
+        self._website_logger = Logger(
+            path = website_log_path,
+            col = ["user", "type", "status"]
+        )
 
         # WebSocket events
         self.socketio.on_event("connect", self.ws_connect)
@@ -27,6 +75,65 @@ class Website:
         self.socketio.on_event("telemetry/start", self.ws_start_stream)
         self.socketio.on_event("telemetry/stop", self.ws_stop_stream)
         self.socketio.on_event("command", self.ws_command)
+
+        self._app = Flask(
+            __name__,
+            template_folder = WEBSITE_TEMPLATE_FOLDER_PATH,
+            static_folder = WEBSITE_STATIC_FOLDER_PATH
+        )
+        self.socketio.init_app(self._app)
+
+        @self._app.get("/api/send_heartbeat")
+        def send_heartbeat():
+            """
+            Heartbeat endpoint to keep track of connected users.
+            """
+            user = request.remote_addr
+            with self._lock:
+                if self._shutdown_flag:
+                    if settings.PRINT_WEBSITE_ERRORS:
+                        print(f"WEBSITE ERROR: User attempted to send heartbeat while website shut down.")
+                    self._website_logger.log_data([user, "error", "Attempted to send heartbeat while website shut down."])
+                    return jsonify({"status": "error", "message": "Website has been shut down"}), 500
+                try:
+                    if user not in self._user_heartbeats:
+                        if settings.PRINT_WEBSITE_STATUS:
+                            print(f"WEBSITE STATUS: User {user} connected.")
+                        self._website_logger.log_data([user, "status", "User connected"])
+                    self._user_heartbeats[user] = time.time()
+                    return jsonify({"status": "success"})
+                except Exception as e:
+                    if settings.PRINT_WEBSITE_ERRORS:
+                        print(f"WEBSITE ERROR: Failed to process heartbeat from {user}: {e}")
+                    self._website_logger.log_data([user, "error", f"Failed to process heartbeat: {e}"])
+                    return jsonify({"status": "error", "message": "Failed to process heartbeat"}), 500  
+
+        if settings.PRINT_WEBSITE_STATUS:
+            print(f"WEBSITE STATUS: Website running...")
+            print(f"WEBSITE INFO: Website id: {id(self)}")
+            print(f"WEBSITE INFO: Controller id: {id(self.flight_computer)}")
+            print(f"WEBSITE INFO: Website port: {self._port}")
+            print(f"WEBSITE INFO: Polling rate: {settings.WEBSITE_THREAD_UPDATE_RATE}Hz")
+            print(f"WEBSITE INFO: Heartbeat timeout: {settings.WEBSITE_HEARTBEAT_TIMEOUT}s")
+            print(f"WEBSITE INFO: Website logger: {str(self._website_logger)}")
+            
+        self._website_logger.log_data(["system", "status", "Server started"])
+        self._website_logger.log_data(["system", "info", f"Website id: {id(self)}"])
+        self._website_logger.log_data(["system", "info", f"Controller id: {id(self.flight_computer)}"])
+        self._website_logger.log_data(["system", "info", f"Website port: {self._port}"])
+        self._website_logger.log_data(["system", "info", f"Polling rate: {settings.WEBSITE_THREAD_UPDATE_RATE}Hz"])
+        self._website_logger.log_data(["system", "info", f"Heartbeat timeout: {settings.WEBSITE_HEARTBEAT_TIMEOUT}s"])
+        self._website_logger.log_data(["system", "info", f"Website logger: {str(self._website_logger)}"])
+
+        # Start background thread to update website data
+        update_website_thread = threading.Thread(target = self._update_website, daemon = True)
+        update_website_thread.start()
+
+        flask_thread = threading.Thread(
+            target = lambda: self.socketio.run(self._app, host = '0.0.0.0', port = self._port, threaded = True),
+            daemon = True
+        )
+        flask_thread.start()
 
     # ---------- helpers ----------
     @staticmethod
@@ -126,7 +233,8 @@ class Website:
                 [int(v) for v in cmd.get("args", [])],
             ),
         }
-
+        if cmd_type is None:
+            raise ValueError(f"Unsupported command type: {cmd_type}")
         command_handler = command_map.get(cmd_type)
         if command_handler is None:
             raise ValueError(f"Unsupported command type: {cmd_type}")
@@ -270,8 +378,18 @@ class Website:
                     last_err_ms = now_ms
             time.sleep(period)
 
-    def run(self, host="0.0.0.0", port=5000):
-        self.socketio.run(self.app, host=host, port=port)
 
-if __name__ == "__main__":
-    Website().run(port=5001)
+    def __del__(self) -> None:
+        self.shutdown()
+
+    def shutdown(self) -> None:
+        """
+        Shuts down the website. After this function is called, calls to other methods will raise an exception.
+        """
+        with self._lock:
+            if self._shutdown_flag:
+                return
+            self._shutdown_flag = True
+            if settings.PRINT_WEBSITE_STATUS:
+                print(f"WEBSITE STATUS: Website shutting down")
+            self._website_logger.log_data(["system", "status", "Server shutting down"])
